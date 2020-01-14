136881104f8eefe7abdc7756fbbdf4359638e546
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 19:44:02 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,15 @@
-print('hello')
-print('print')
+#Incremental Development 
+
+def distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    print('dx is', dx)
+    print('dy is', dy)
+    return 0.0
+  
+def distance(x1, y1, x2, y2):
+    dx = x2 - x1
+    dy = y2 - y1
+    dsquared = dx**2 + dy**2
+    print('dsquared is: ', dsquared)
+    return 0.0
